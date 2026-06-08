da5d0edd15592c8caee9f8ff06f20ce5600d4c58
Charles Lutz <103493003+CharlesCarMichaelLutz@users.noreply.github.com>
2026-06-08 18:30:47 +0800

    created sidebar for AppLayout

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -11,7 +11,45 @@ export function AppLayout() {
     return (
         <>
             <div className="app-layout-container">
-                <aside className={`sidebar ${isCollapsed ? "collapsed" : ""}`}></aside>
+                <aside className={`sidebar ${isCollapsed ? "collapsed" : ""}`}>
+                    <div className="sidebar-container">
+                        <div className="sidebar-user">
+                                <img height={60} width={40} className="user-identicon"/>
+                                <h2 className="sidebar-username">UserFive</h2>
+                        </div>
+                        <div className="sidebar-menu">
+                            <nav>
+                                <h3>Year</h3>
+                                <ul>
+                                    <li>2026
+                                        <ul>
+                                            <li>January</li>
+                                            <li>February</li>
+                                            <li>March</li>
+                                            <li>April</li>
+                                        </ul>
+                                    </li>
+                                    <li>2025
+                                        <ul>
+                                            <li>January</li>
+                                            <li>February</li>
+                                            <li>March</li>
+                                            <li>April</li>
+                                        </ul>
+                                    </li>
+                                    <li>2024
+                                        <ul>
+                                            <li>January</li>
+                                            <li>February</li>
+                                            <li>March</li>
+                                            <li>April</li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </aside>
                 <div className="right-panel">
                     <header className="header-panel">
                             <div className="header-left">
@@ -25,7 +63,7 @@ export function AppLayout() {
                                     <img height={40} width={34} className="currency-image" />
                                     <img height={40} width={34} className="currency-image" />
                                 </div>
-                                <h2>Spend Aware</h2>
+                                <h1>Spend Aware</h1>
                                 <div className="currency-group">
                                     <img height={40} width={34} className="currency-image" />
                                     <img height={40} width={34} className="currency-image" />
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,4 +1,75 @@
 
 export function Dashboard() {
-    return <h1>Dashboard</h1>
+    return (
+        <>
+            <section className="dashboard">
+                <div className="dashboard-top">
+                    <form>
+                        <label >Date:</label>
+                        <input
+                            type="date"
+                            name="date"
+                        />
+                        <label >Description:</label>
+                        <input
+                            type="text"
+                            name="description"
+                            placeholder='enter description' />
+                        <label >Place:</label>
+                        <input
+                            type="text"
+                            name="place"
+                            placeholder='enter place' />
+                        <label >Amount:</label>
+                        <input
+                            type="number"
+                            name="amount"
+                            placeholder='enter amount' />
+                        <input
+                            type="submit"
+                            className="submit"
+                            id="submit" />
+                    </form>
+                </div>
+                <div className="dashboard-middle">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Place</th>
+                            <th>Description</th>
+                            <th>Amount</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>Date</td>
+                                <td>Place</td>
+                                <td>Description</td>
+                                <td>Amount</td>
+                                <td>
+                                    <button>Edit</button>
+                                </td>
+                                <td>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        </tbody>
+
+                        <tfoot>
+                        <tr>
+                            <th>Month Name</th>
+                            <th>Total</th>
+                            <th>Amount</th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+                <div className="dashboard-bottom">Pagination Bar</div>
+            </section>
+        </>
+    )
 }
