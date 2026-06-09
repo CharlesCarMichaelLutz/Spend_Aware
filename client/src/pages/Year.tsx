9aff2bc84c3bf4496931c4dc5a2ef260010306bc
Charles Lutz <103493003+CharlesCarMichaelLutz@users.noreply.github.com>
2026-06-09 12:55:16 +0800

    created card grid for year page

--- client/src/pages/Year.tsx
+++ client/src/pages/Year.tsx
@@ -1,3 +1,22 @@
 export function Year() {
-  return <h1>Year</h1>;
+  return (
+      <>
+        <div className="month-cards-wrapper">
+          <div className="month-cards-grid">
+            <div className="month-card">January</div>
+            <div className="month-card">February</div>
+            <div className="month-card">March</div>
+            <div className="month-card">April</div>
+            <div className="month-card">May</div>
+            <div className="month-card">June</div>
+            <div className="month-card">July</div>
+            <div className="month-card">August</div>
+            <div className="month-card">September</div>
+            <div className="month-card">October</div>
+            <div className="month-card">November</div>
+            <div className="month-card">December</div>
+          </div>
+        </div>
+      </>
+  );
 }
