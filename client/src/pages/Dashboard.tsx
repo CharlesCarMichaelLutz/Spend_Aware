058e61750dbab200baa89c2f245c459758778a09
Charles Lutz <103493003+CharlesCarMichaelLutz@users.noreply.github.com>
2026-06-12 14:32:49 +0800

    tested mock endpoints with JSON server for Dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,66 +1,95 @@
 import {useEffect, useState} from "react";
-import { User, Expense } from "../types/types"
+import type { User, Expense } from "../types/types"
 
 export function Dashboard() {
     const [expenseData, setExpenseData] = useState<Expense[]>([]);
     const [user, setUser] = useState<User | null>(null);
+
+    const id = 1;
+    // const id = 2;
+    //const id = 3;
     
-    // function GetCurrentMonthAndYear {
-    //    
-    // }
     
     useEffect(() => {
-        async function GetUserById(user_id: number) {
+        async function getUserById(id: number):Promise<void> {
             try {
-                const response = await fetch(`https://api.github.com/users/${year}/${month}/${year}`, {})
+                const response = await fetch(`http://localhost:8000/users/${id}`)
                 
                 if(!response.ok) {
                     throw new Error("Failed to fetch user");
                 }
                 
                 const userData: User = await response.json();
-                setUser({user:userData})
+                setUser({userData})
+                console.log("user :", userData) 
             }
             catch(err) {
                 console.error(err);
             }
         }
-        GetUserById(1)
-    },[user_id])
+        getUserById(id)
+    },[id])
     
     //pass in the start/end date, and user_id
     
     useEffect(() => {
-        if(user != null) {
+        if(!user) return; 
             //use the year and month to get expenses for that user from data source
-            async function GetExpensesByUserId(start: Date, end: Date, user_id: number) {
+            async function getExpensesByUserId(start: Date, end: Date, id: number):Promise<void> {
                 try {
+                    const startStr = start.toISOString().split('T')[0];
+                    const endStr = end.toISOString().split('T')[0];
+                    
                     //filter expense for specific date with TS
-                    const response = await fetch(`https://api.github.com/users/${year}/${month}/${year}`, {})
+                    const response = await fetch(`http://localhost:8000/expenses?user_id=${id}&created_at_gt=${startStr}&created_at_lte=${endStr}`)
     
                     if(!response.ok) {
                         throw new Error("Failed to fetch expenses");
                     }
     
-                    const expenseList: Expense[] = await response.json();
-                    //set the expense in state with setExpenseData
-                    setExpenseData({expenseList:expenseList})
+                    const data = await response.json();
+                    console.log("API response:", data);
+                    console.log("Is array?", Array.isArray(data));
+                    setExpenseData(data || [])
                 }
                 catch(err) {
                     console.error(err);
                 }
-            }
         }
-        GetExpensesByUserId(firstDay, lastDay, user.id);
-    }, [user_id])
+        getExpensesByUserId(firstDay, lastDay, id);
+    }, [user])
+
+    //previous month March
+    // const now = new Date();
+    // const year = now.getFullYear()
+    // const firstDay = new Date(year, now.getMonth() - 3, 1)
+    // console.log(firstDay)
+    // const lastDay = new Date(year, now.getMonth() - 2, 1)
+    // console.log(lastDay)
     
-    const date: Date = new Date();
-    console.log(date)
-    const firstDay: Date = new Date(date.getFullYear(), date.getMonth(), 1);
+    //previous month April
+    // const now = new Date();
+    // const year = now.getFullYear()
+    // const firstDay = new Date(year, now.getMonth() - 2, 1)
+    // console.log(firstDay)
+    // const lastDay = new Date(year, now.getMonth() - 1, 1)
+    // console.log(lastDay)
+    
+    //previous month May
+    // const now = new Date();
+    // const year = now.getFullYear()
+    // const firstDay = new Date(year, now.getMonth() - 1, 1)
+    // console.log(firstDay)
+    // const lastDay = new Date(year, now.getMonth() , 1)
+    // console.log(lastDay)
+    
+    //current month June
+    const now = new Date();
+    const year = now.getFullYear()
+    const firstDay = new Date(year, now.getMonth(), 1)
     console.log(firstDay)
-    const lastDay: Date = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    const lastDay = new Date(year, now.getMonth() + 1, 1)
     console.log(lastDay)
-    
     
     //display expenses from above request in table 
     
