82d7f9b6ae48df4bfbab4d8a0381ca4c7d49c6e0
sub-zero <sub-zero@example.com>
2026-09-03 07:02:12 +0800

    disabled month buttons based on signup date

--- client/src/pages/Year.tsx
+++ client/src/pages/Year.tsx
@@ -1,13 +1,29 @@
 import { Outlet, useParams, NavLink } from "react-router";
+import { useStore } from "../store/useStore.ts"
 
 export function Year() {
     const { year } = useParams()
+    const user = useStore(state => state.auth)
     
     //disable the card and button for months before the user signup date
-    //disable the card and  buttons for months in the future 
     
     //if the user clicks on the current month redirect to dashboard component 
     // which shows the current month
+    
+    const now = new Date();
+    const currMonth = now.getMonth()
+    // console.log("currentMonth :", currMonth)
+
+    const signupDate = user.createdAt
+    // console.log("sign up date:", signupDate)
+    
+    const date = new Date(signupDate)
+    // console.log("date:", date)
+    
+    const pastMonthIndex = date.getMonth()
+    const pastYear = date.getFullYear()
+    
+    // console.log("past month:", pastMonthIndex)
     
   const months : string[] = [
       "January",
@@ -24,26 +40,58 @@ export function Year() {
       "December"
   ]
   
-  return (
-      <>
-          <div className="year-wrapper">
-              <div className="year-title-wrapper">
-                  <h3 className="year-title">Year:{year}</h3>
-                  <button>Get Report</button>
-              </div>
-              <div className="month-cards-grid">
-                  {months.map(month => (
-                          <NavLink className="month-card" to={`/dashboard/${year}/${month}`}>
-                              {month}
-                          </NavLink>
-                  ))}
-                </div>
-          </div>
-          <div>
-              < Outlet />
-          </div>
-      </>
-  );
+//   return (
+//       <>
+//           <div className="year-wrapper">
+//               <div className="year-title-wrapper">
+//                   <h3 className="year-title">Year:{year}</h3>
+//                   <button>Get Report</button>
+//               </div>
+//               <div className="month-cards-grid">
+//                   {months.map(month => (
+//                           <NavLink className="month-card" to={`/dashboard/${year}/${month}`}>
+//                               {month}
+//                           </NavLink>
+//                   ))}
+//                 </div>
+//           </div>
+//           <div>
+//               < Outlet />
+//           </div>
+//       </>
+//   );
+// }
+
+return (
+    <>
+        <div className="year-wrapper">
+            <div className="year-title-wrapper">
+                <h3 className="year-title">Year:{year}</h3>
+                <button>Get Report</button>
+            </div>
+            <div className="month-cards-grid">
+                {months.map(month => (
+                    // console.log("month: ", month),
+                    // console.log("index month :", months.indexOf(month)),
+                    // console.log(months.indexOf(month), "curr:",  currMonth, "past :",  pastMonthIndex),
+                        //disable the card buttons for months in the future 
+                    months.indexOf(month) > currMonth || months.indexOf(month) < pastMonthIndex ? (
+                        <NavLink key={month} className="disabled-month-card" onClick={(e) => e.preventDefault()} to={`/dashboard/${year}/${month}`}>
+                            {month}
+                        </NavLink> 
+                        ) :
+                        (
+                            <NavLink key={month} className="month-card" to={`/dashboard/${year}/${month}`}>
+                                {month}
+                            </NavLink>  )
+                        ))}
+            </div>
+        </div>
+        <div>
+            < Outlet />
+        </div>
+    </>
+);
 }
 
 // only want to get the months the user has an expense for not empty ones so cards are rendered correctly
